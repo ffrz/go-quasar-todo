eae7dd44698b597b58e3f47555ecdd78622181f8
Fahmi Fauzi Rahman <fahmifauzirahman@gmail.com>
2025-11-02 22:28:25 +0700

    remove tmp files

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -30,7 +30,7 @@ func main() {
 
 	root := app.Group("/")
 	root.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!!")
+		return c.SendString("Hello, World!!!")
 	})
 
 	api.RegisterRoutes(app, db)
